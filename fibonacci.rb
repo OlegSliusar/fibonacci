9091190729a49c76391ac520c9647bae5dc61d7e
Oleh Sliusar <oleh@olehsliusar.com>
2017-03-22 13:56:52 +0200

    Refactor methods

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,14 +1,11 @@
 def fibs(n)
   f1 = 0; f2 = 1
-  n.times do
-    f1, f2 = f2, f1 + f2
-  end
+  n.times { f1, f2 = f2, f1 + f2 }
   f1
 end
 
 def fibs_rec(n)
-  n = fibs_rec(n - 1) + fibs_rec(n - 2) if n > 1
-  n
+  n > 1 ? n = fibs_rec(n - 1) + fibs_rec(n - 2) : n
 end
 
 puts "Actual: #{fibs(0)}, should be 0"
