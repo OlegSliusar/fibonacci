746f89722c348b4027f2d7e39e0741acafaa248f
Oleh Sliusar <oleh@olehsliusar.com>
2017-03-22 16:24:09 +0200

    Optimize recursion method

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,11 +1,16 @@
+require 'benchmark'
 def fibs(n)
   f1 = 0; f2 = 1
   n.times { f1, f2 = f2, f1 + f2 }
   f1
 end
 
+def fib3(n, i, j)
+  n == 0 ? i : fib3(n - 1, i + j, i)
+end
+
 def fibs_rec(n)
-  n > 1 ? n = fibs_rec(n - 1) + fibs_rec(n - 2) : n
+  fib3(n, 0, 1)
 end
 
 puts "Actual: #{fibs(0)}, should be 0"
@@ -28,4 +33,7 @@ puts "Actual: #{fibs_rec(5)}, should be 5"
 puts "Actual: #{fibs_rec(6)}, should be 8"
 puts "Actual: #{fibs_rec(7)}, should be 13"
 puts "Actual: #{fibs_rec(8)}, should be 21"
-puts "Actual: #{fibs_rec(39)}, should be 63245986"
+b = Benchmark.measure do
+  puts "Actual: #{fibs_rec(39)}, should be 63245986"
+end
+puts b
